1e9f87d02a50e77358524a5fa94f63acb30f1a00
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-05-07 19:03:14 +0200

    Page Restauration Photos desktop

---
+++ menuTopNav.js
@@ -0,0 +1,12 @@
+// Menu  burger //
+
+function checkIsBurger(element) => {
+    element.addEventListener('click', () => {
+        element.classList.toggle('isBurger');
+    });
+};
+
+checkIsBurger(document.getElementById('element1'));
+checkIsBurger(document.getElementById('element2'));
+checkIsBurger(document.getElementById('element3'));
+checkIsBurger(document.getElementById('element4'));
