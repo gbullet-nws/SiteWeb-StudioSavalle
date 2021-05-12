2145a8d680cb905cba97c55320894d703ed89666
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-05-12 13:41:34 +0200

    photoANTS Flexbox, drone desktop

--- menuTopNav.js
+++ menuTopNav.js
@@ -1,9 +1,40 @@
+////-----------////
 // Sticky HEADER //
+////-----------////
+
+// lorsque la page est scroll, appelle la fonction stickyHeader
+window.onscroll = function() 
+{
+    stickyHeader();
+};
+
+// secltion de l'élement HTML par son id --> Header
+var header = document.getElementsByClassName("header");
+
+// Stocker la position offsetTop du header
+var sticky = header.offsetTop;
+
+// ajoute ou retire la class sticky à l'élément Header selon sa position sur la page
+function stickyHeader() 
+{
+  if (window.pageYOffset > sticky) 
+  {
+    header.classList.add("sticky");
+  } 
+  else 
+  {
+    header.classList.remove("sticky");
+  }
+}
 
 
-// Menu  burger //
+/* 
 
-function checkIsBurger(element) => 
+////---------------------------////
+// //----// Menu Burger //----// //
+////---------------------------////
+
+function checkIsBurger(element) =>
 {
     element.addEventListener('click', () => 
     {
@@ -15,3 +46,5 @@ checkIsBurger(document.getElementById('element1'));
 checkIsBurger(document.getElementById('element2'));
 checkIsBurger(document.getElementById('element3'));
 checkIsBurger(document.getElementById('element4'));
+
+*/
