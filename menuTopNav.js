c52d76b9a92330497a367889c095fa54f0fc318f
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-06-04 15:33:30 +0200

    menu burger sticky

--- menuTopNav.js
+++ menuTopNav.js
@@ -2,32 +2,6 @@
 // -- // Sticky HEADER // -- //
 ////-----------------------////
 
-// lorsque la page est scroll, appelle la fonction stickyHeader
-window.onscroll = function() 
-{
-  stickyHeader();
-};
-
-// selection de l'élement HTML par son id -> header
-let navBar = document.getElementById("NavBar");
-
-// Stocker la position offsetTop du menu de nav
-let sticky = navBar.offsetTop;
-
-// ajoute ou retire la class sticky à l'élément NavBar selon sa position sur la page
-function stickyHeader() 
-{
-  if (window.pageYOffset > sticky) 
-  {
-    navBar.classList.add("sticky");
-  } 
-  else 
-  {
-    navBar.classList.remove("sticky");
-  }
-}
-
-
 
 ////---------------------------////
 // //----// Menu Burger //----// //
@@ -35,20 +9,3 @@ function stickyHeader()
 
 
 
-
-/* 
-
-function checkIsBurger(element) =>
-{
-    element.addEventListener('click', () => 
-    {
-        element.classList.toggle('isBurger');
-    });
-};
-
-checkIsBurger(document.getElementById('element1'));
-checkIsBurger(document.getElementById('element2'));
-checkIsBurger(document.getElementById('element3'));
-checkIsBurger(document.getElementById('element4'));
-
-*/
