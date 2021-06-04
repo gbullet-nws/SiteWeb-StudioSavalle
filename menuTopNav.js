f0a7a7a9a84538052c4cfe4b0dee2f0d7e4556a0
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-06-04 14:22:04 +0200

    menu-topNav-burger --> mobile & tablette V.1

--- menuTopNav.js
+++ menuTopNav.js
@@ -28,11 +28,23 @@ function stickyHeader()
 }
 
 
-/* 
-
 ////---------------------------////
 // //----// Menu Burger //----// //
 ////---------------------------////
+
+var menuButton = document.querySelector('#menu-button');
+var menu = document.querySelector('#menu');
+
+// show or hide
+menuButton.addEventListener('click',function()
+{
+  menu.classList.toggle('show-menu');
+  menuButton.classList.toggle('close');
+});
+
+
+
+/* 
 
 function checkIsBurger(element) =>
 {
