a821d51d3812880b82c706092e85c57ba942e3a0
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-06-28 19:28:34 +0200

    Slider SLICK --> impression3D

--- util.js
+++ util.js
@@ -38,3 +38,64 @@ function reduireQuantite()
         document.getElementById('quantiteProduit').value = --quantiteProduit;
     }
 }
+
+/* ------------------------------------------ *//* ------------------------------------------ */
+/* ------------------------------------------ *//* ------------------------------------------ */
+
+  /////--------------------------/////
+ // //----// SLICK SLIDER //----// //
+/////--------------------------/////
+
+$(function()
+{
+    $('.slider-slick-1').slick(
+    {
+        centerMode: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        centerPadding: '60px',
+        variableWidth: true,
+        variableheight: true,
+        mobileFirst: true,
+        responsive:
+        [
+            {
+                breakpoint: 1024,
+                settings: 
+                {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    centerPadding: '0px',
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: 
+                {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 300,
+                settings: 
+                {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            }
+        ],
+    });
+});
+
+$(window).resize(function()
+{
+    $('.slider-slick-1')[0].slick.refresh();
+});
